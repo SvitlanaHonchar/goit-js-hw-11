4091a398004a7fbedbe3497d0418befc018e47b6
SvitlanaHonchar <snidlty@gmail.com>
2023-01-22 20:15:27 +0200

    styles

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import { getPhotos } from './getPhotos';
 import { PixabayAPI } from './getPhotos';
+import { createGalleryCards } from './createGalleryCards';
 
 const pixabayAPI = new PixabayAPI();
 
 const formEl = document.querySelector('.search-form');
+const galleryListEl = document.querySelector('.gallery');
 
 function onSubmit(e) {
   e.preventDefault();
@@ -11,7 +12,10 @@ function onSubmit(e) {
   console.log(query);
   pixabayAPI
     .getPhotos(query)
-    .then(data => console.log(data))
+    .then(data => {
+      galleryListEl.innerHTML = createGalleryCards(data);
+      console.log(data);
+    })
     .catch(err => console.dir(err));
   //   console.log(data);
 }
